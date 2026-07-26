f587d1ff1fd73f838e52a37a17720e7a249d9bdc
Suphasit07 <suphasit.k@ku.th>
2026-07-26 14:35:13 +0700

    Add files via upload

--- BoundedStack.java
+++ BoundedStack.java
@@ -1,5 +1,33 @@
+import java.util.*;
+/*
+ * 
+ * BoundedStack คือ ..
+ */
 public class BoundedStack{
+    private  final List<String> element;
+    private final int capacity;
+    //AF(elements,cpacity =
+    //RI
+    // -
+    // - 
+    
+    /**
+     * 
+     * @param capacity
+     */
+    public BoundedStack(int capacity){
+    this.element = new ArrayList<>();
+    this.capacity = capacity;
+   }
 
+
+   /**
+    * 
+    * @param s
+    */
+   public void push(String  s){
+
+   }
 
 
 
