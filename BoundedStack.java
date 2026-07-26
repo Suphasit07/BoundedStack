a03a68aae50e00c2497398779b4f64c57bbc5b07
Suphasit07 <suphasit.k@ku.th>
2026-07-26 15:53:49 +0700

    version1.4

--- BoundedStack.java
+++ BoundedStack.java
@@ -4,27 +4,31 @@ import java.util.*;
  * BoundedStack คือ ..
  */
 public class BoundedStack{
-    private final List<String> User;
-    private final int Ticket;
-    private final List<String> Seat;
+    private final List<String> user;
+    private final List<String> password;
+    private final int ticket;
+    private final List<String> seat;
     //Abstraction Function:
-    //AF(User,Ticket = กดบัตรคอนเสิร์ตแบบตามลำดับ User ที่เข้ามาก่อน
+    //AF(User,Ticket,Seat = กดบัตรคอนเสิร์ตแบบตามลำดับ User และ Seat ตามลำดับ)
 
     // Representation Invariant:
     //RI(User) = ชื่อ User ห้ามซ้ำกัน
     //RI(User) = ไม่มีชื่อ User ที่เป็นสตริงว่าง
-    //RI(User) = 
+    //RI(User) = ตำแหน่ง Seat ต้องไม่ซ้ำกัน
     //RI(User) = User 1 คน ซื้อ Ticket ได้ 1 ใบ
-    //RI(User) = da[sdkasdpkad]
+    //RI(User) = 
+    //RI(User) =
     // - 
     
     /**
      * 
      * @param capacity
      */
-    public BoundedStack(int capacity){
-    this.User = new ArrayList<>();
-    this.Ticket = capacity;
+    public BoundedStack(int Ticket){
+    this.user = new ArrayList<>();
+    this.password = new ArrayList<>();
+    this.ticket = Ticket;
+    this.seat = new ArrayList<>();
    }
 
 
